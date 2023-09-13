35e3892e816745f251a0a0e31866396fbb2902c4
knblo <knblo@LAPTOP-N9N98F1S>
2023-09-12 20:07:51 -0600

    Add project files.

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,22 @@
+/*************************************************************
+ * 1. Name:
+ *      Kevin Blomquist
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      A simple program to display a message on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      Remebering how to include iostream and using namespaces
+ * 5. How long did it take for you to complete the assignment?
+ *       > 1 min
+ *****************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
